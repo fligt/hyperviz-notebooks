d47f6b0bf74d6fda8644619872ba4766ed1f8c72
Koen van Tilburg <koenvantilburg@gmail.com>
2026-08-13 17:09:26 +0200

    dashboard with tif and multiple ROI spectra

--- notebooks/koen/01_boxedit_using_plotly.py
+++ notebooks/koen/01_boxedit_using_plotly.py
@@ -18,11 +18,12 @@
 # https://plotly.com/python/selections/
 
 # %%
-import dash
 from dash import Dash, dcc, html, Input, Output, no_update, callback
 import plotly.express as px
 import numpy as np
 from fairdatanow import data_now
+from skimage import io
+import datashader as ds
 
 # %%
 url = 'https://laboppad.nl/ukiyo-e-world' 
@@ -40,10 +41,26 @@ RV-1-4470-27 = ".*RIS/interim/.*RV-1-4470-27.*[.]npz"
 RV-360-2345g = ".*RIS/interim/.*RV-360-2345g.*[.]npz"
 RV-360-2359-2 = ".*RIS/interim/.*RV-360-2359-2.*[.]npz"
 RV-360-6886 = ".*RIS/interim/.*RV-360-6886.*[.]npz"
+
+[data.tif]
+# these are regexes that download all .tif files per object number 
+RV-1-4468-544 = ".*akama.*RV-1-4468-544[.]tif"
+RV-1-4469-484 = ".*akama.*RV-1-4469-484[.]tif"
+RV-1-4469-58 = ".*akama.*RV-1-4469-58[.]tif"
+RV-1-4469-x6 = ".*akama.*RV-1-4469-x6[.]tif"
+RV-1-4469Q = ".*akama.*RV-1-4469Q[.]tif"
+RV-1-4470-12 = ".*akama.*RV-1-4470-12[.]tif"
+RV-1-4470-27 = ".*akama.*1-4470-27[.]tif"      # TIF NAME WITHOUT RV prefix!  
+RV-360-2345g = ".*akama.*RV-360-2345-?g[.]tif"
+RV-360-2359-2 = ".*akama.*RV-360-2359-2[.]tif"
+RV-360-6886 = ".*akama.*RV-360-6886[.]tif"
 '''
 
 # %%
 data = data_now(url, toml_txt)
+
+# %%
+tif_file = data['tif']['RV-1-4470-27'][0]
 
 # %%
 npz_file = data['npz']['RV-1-4470-27'][0]
@@ -63,16 +80,22 @@ pseudo_rgb = cube[:,:, [70, 53, 19]]
 # %%
 
 # Base figures
+img = io.imread(tif_file)
+fig_tif  = px.imshow(img)
 fig_img = px.imshow(pseudo_rgb, binary_string=True).update_layout(dragmode="drawrect")
 fig_spec = px.line(x=wavelengths, y=cube.mean(axis=(0, 1)), labels={"x": "Wavelength", "y": "Intensity"})
 
 app = Dash(__name__)
 app.layout = html.Div(
     [
-        html.H3("Drag a rectangle to show the mean spectrum of the ROI"),
+        html.H3("First Dashboard for ROI's"),
         html.Div(
-            [dcc.Graph(id="pseudo_rgb", figure=fig_img, config={"modeBarButtonsToAdd": ["drawrect", "eraseshape"]})],
-            style={"width": "40%", "display": "inline-block"},
+            [dcc.Graph(id="tif_rgb", figure=fig_tif, config={'scrollZoom':True})],
+            style={"width": "30%", "display": "inline-block"},
+        ),
+        html.Div(
+            [dcc.Graph(id="pseudo_rgb", figure=fig_img, config={"modeBarButtonsToAdd": ["drawrect", "eraseshape"], 'scrollZoom':True})],
+            style={"width": "30%", "display": "inline-block"},
         ),
         html.Div(
             [dcc.Graph(id="spectrum", figure=fig_spec)],
@@ -84,7 +107,6 @@ app.layout = html.Div(
 
 @callback(
     Output("spectrum", "figure"),
-    Output("text-example-output", "children"),
     Input("pseudo_rgb", "relayoutData"),
     prevent_initial_call=True
 )
@@ -93,25 +115,36 @@ def on_new_annotation(relayout_data):
     if not shapes:
         return no_update
 
-    s = shapes[-1]
-    
-    x0, x1 = sorted([max(0, min(w, int(s["x0"]))), max(0, min(w, int(s["x1"])))])
-    y0, y1 = sorted([max(0, min(h, int(s["y0"]))), max(0, min(h, int(s["y1"])))])
+    lines = []
 
-    roi_cube = cube[y0:y1, x0:x1, :]
-    if roi_cube.size == 0:
-        return no_update
+    for s in shapes:
+    
+        x0, x1 = sorted([max(0, min(w, int(s["x0"]))), max(0, min(w, int(s["x1"])))])
+        y0, y1 = sorted([max(0, min(h, int(s["y0"]))), max(0, min(h, int(s["y1"])))])
+
+        roi_cube = cube[y0:y1, x0:x1, :]
+        if roi_cube.size == 0:
+            return no_update
+        lines.append(roi_cube.mean(axis=(0,1)))
 
     fig = px.line(
         x=wavelengths, 
-        y=roi_cube.mean(axis=(0, 1)), 
+        y=lines, 
         labels={"x": "Wavelength", "y": "Intensity"},
         title=f"ROI Mean Spectrum ({x1-x0}x{y1-y0} px)"
     )
 
-    coords = f'x0:{x0}, x1:{x1}, y0:{y0}, y1:{y1}'
+    #coords = f'x0:{x0}, x1:{x1}, y0:{y0}, y1:{y1}'
 
-    return fig, coords
+    return fig
+
+@callback(
+    Output(component_id='text-example-output', component_property='children'),
+    Input('pseudo_rgb', 'clickData'),
+    prevent_initial_call=True
+)
+def on_rgb_click(click_data):
+    return str(click_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
